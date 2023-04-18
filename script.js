5cee9c1d6890c0aa07cc9374bc3ed660f9425bec
AmeyPank <pankantiameya@.com>
2023-04-19 01:05:56 +0530

    Plindrome Checker

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 // complete the given function
 
 function palindrome(str){
-
+ // Remove non-alphanumeric characters and convert to lowercase
+ str = str.replace(/[\W_]/g, "").toLowerCase();
+ // Reverse the string and compare it to the original
+ return str === str.split("").reverse().join("");
 }
 module.exports = palindrome
